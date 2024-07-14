7fec0bf619c36bab901cd92b22b371c2268dad23
Gizem Karyağdı <125547840+gizemkaryagdi@users.noreply.github.com>
2024-07-14 13:54:03 +0300

    Delete autogen/Project 1- Fetch_Github_Stars/coding directory

--- autogen/Project 1- Fetch_Github_Stars/coding/autogen_star_count.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def get_star_count():
-    url="https//api.github.com/repos/microsoft/Autogen"
-    response=requests.get(url)
-    data=response.json()
-    return data['stargazers_count']
-print(f"Microsoft's Autogen has {get_star_count()} stars on Github.")
